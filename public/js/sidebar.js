0f31a1bd56d1142cc7ced49af5c687b5c6c4b3f6
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-04-27 23:44:20 -0300

    sidebar fechada e implementação do javascript

---
+++ public/js/sidebar.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.getElementById('sidebar');
+    const menuButton = document.getElementById('menu');
+    const items = document.querySelectorAll('.item');
+    
+    menuButton.addEventListener('click', () => {
+        sidebar.classList.toggle('fechada');
+
+        const menuIcon = menuButton.querySelector('img');
+        if (sidebar.classList.contains('fechada')) {
+            menuIcon.src = '/public/assets/sidebar/MenuIcon.png';
+        } else {
+            menuIcon.src = '/public/assets/sidebar/FecharIcon.png';
+        }
+    });
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            items.forEach(i => i.classList.remove('active'));
+            item.classList.add('active');
+        });
+    });
+});
