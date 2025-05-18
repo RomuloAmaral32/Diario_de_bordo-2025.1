04b8e4693e8c77b7db70b0f29172de6e5faec765
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-05-18 15:25:22 -0300

    implementando a logica do modal delete user

--- public/js/user_list.js
+++ public/js/user_list.js
@@ -9,3 +9,13 @@ function fecharModalViewUser(idview_user){
     document.getElementById(idview_user).style.display = "none";
     filtro.style.display = "none";
 }
+
+function abrirModalDeleteUser(idmodal_delete_box){
+    document.getElementById(idmodal_delete_box).style.display = "flex";
+    filtro.style.display = "block";
+}
+
+function fecharModalDeleteUser(idmodal_delete_box){
+    document.getElementById(idmodal_delete_box).style.display = "none";
+    filtro.style.display = "none";
+}
