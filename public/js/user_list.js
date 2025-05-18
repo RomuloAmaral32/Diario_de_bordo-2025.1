7b383e40bb08fd7665190c3f3a86da580c00ada0
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-05-18 17:31:52 -0300

    implementando logica do modal edit user

--- public/js/user_list.js
+++ public/js/user_list.js
@@ -1,5 +1,6 @@
 const filtro = document.getElementById('filtro')
 
+//------------------ Modal View User ------------------------
 function abrirModalViewUser(idview_user){
     document.getElementById(idview_user).style.display = "flex";
     filtro.style.display = "block";
@@ -9,6 +10,28 @@ function fecharModalViewUser(idview_user){
     document.getElementById(idview_user).style.display = "none";
     filtro.style.display = "none";
 }
+
+//------------------------------------------------------------
+
+//------------------ Modal Create New User ------------------------
+
+function abrirModalCreateUser(idmodal_new_user){
+    document.getElementById(idmodal_new_user).style.display = "flex";
+    filtro.style.display = "block";
+}
+
+function fecharModalCreateUser(idmodal_new_user){
+    document.getElementById(idmodal_new_user).style.display = "none";
+    filtro.style.display = "none";
+}
+
+//------------------------------------------------------------
+
+//------------------ Modal Edit User ------------------------
+
+//------------------------------------------------------------
+
+//------------------ Modal Delete User ------------------------
 
 function abrirModalDeleteUser(idmodal_delete_box){
     document.getElementById(idmodal_delete_box).style.display = "flex";
