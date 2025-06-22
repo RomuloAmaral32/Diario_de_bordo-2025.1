7411e0fe41248d5e266455b7710de39ae4e618b9
Vini-LM <vinicius.matos@estudante.ufjf.br>
2025-06-22 19:08:03 -0300

    Altera nome do css da view post individual

--- app/views/site/view_post.view.php
+++ app/views/site/view_post.view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seu post</title>
-    <link rel="stylesheet" href="/public/css/styles_view_post.css">
+    <link rel="stylesheet" href="/public/css/individual_view_post.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap"
